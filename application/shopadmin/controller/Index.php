b951588f98f413ad060d91c3a753216842202f21
Beckyang <snryid@163.com>
2017-12-16 10:18:44 +0800

    更新后台logo,添加开发说明文件

--- application/shopadmin/controller/Index.php
+++ application/shopadmin/controller/Index.php
@@ -11,9 +11,36 @@
 */
 namespace app\shopadmin\controller;
 
+use think\db;
+use think\Request;
+
 class Index extends Common
 {
 	function index(){
+		$request = Request::instance();
+        $ip      = $request->ip();
+
+        $info = array(
+            'language'             => $_SERVER['HTTP_ACCEPT_LANGUAGE'],
+            'OS'                   => PHP_OS,
+            'server_software'      => $_SERVER["SERVER_SOFTWARE"],
+            'PHP_move'             => php_sapi_name(),
+            'remote_port'          => $_SERVER['SERVER_PORT'],
+            'ThinkPHP'             => THINK_VERSION,
+            'updata'               => ini_get('upload_max_filesize'),
+            'overtime'             => ini_get('max_execution_time') . '秒',
+            'severTime'            => date("Y年n月j日 H:i:s"),
+            'BJtime'               => gmdate("Y年n月j日 H:i:s", time() + 8 * 3600),
+            'ServerName'           => $_SERVER['SERVER_NAME'],
+            'IP'                   => gethostbyname($_SERVER['SERVER_NAME']),
+            'memory'               => round((disk_free_space(".") / (1024 * 1024)), 2) . 'M',
+            'register_globals'     => get_cfg_var("register_globals") == "1" ? "ON" : "OFF",
+            'magic_quotes_gpc'     => (1 === get_magic_quotes_gpc()) ? 'YES' : 'NO',
+            'magic_quotes_runtime' => (1 === get_magic_quotes_runtime()) ? 'YES' : 'NO',
+        );
+
+        $this->assign('info', $info);
+        $this->assign('ip', $ip);
 		return view();
 	}
 }
