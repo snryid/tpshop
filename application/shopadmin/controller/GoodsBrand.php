ab8c8ce0fc83e2db33002b842663f4cf7dcf4c49
Beckyang <snryid@163.com>
2017-12-18 15:14:37 +0800

    更新

--- application/shopadmin/controller/GoodsBrand.php
+++ application/shopadmin/controller/GoodsBrand.php
@@ -26,6 +26,13 @@ class GoodsBrand extends Common
 
     public function add()
     {
+        if (request()->isPost()) {
+            $data = input('post.');
+            dump($data);
+            if ((($_FILES['brand_img']['type'] == "image/jpeg") || ($_FILES['brand_img']['type'] == "image/png")) && ($_FILES['brand_img']['size'] < 200000) ) {
+                
+            }
+        }
     	
         return view('add');
     }
@@ -74,5 +81,24 @@ class GoodsBrand extends Common
         
     }
 
+
+    public function upload(){
+    // 获取表单上传文件 例如上传了001.jpg
+    $file = request()->file('image');
+    // 移动到框架应用根目录/public/uploads/ 目录下
+    $info = $file->validate(['size'=>15678,'ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+    if($info){
+        // 成功上传后 获取上传信息
+        // 输出 jpg
+        echo $info->getExtension();
+        // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+        echo $info->getSaveName();
+        // 输出 42a79759f284b767dfcb2a0197904287.jpg
+        echo $info->getFilename(); 
+    }else{
+        // 上传失败获取错误信息
+        echo $file->getError();
+    }
+}
 
 }
